c549dd5c4a71a7aae1a6018937c500dec3ab0262
chris <chrishethertonweb@gmail.com>
2022-07-11 11:46:42 +0200

    set only one if statement instead of two in exercise 1

--- solution.js
+++ solution.js
@@ -1,14 +1,19 @@
-let int1 = 36;
-let int2 = 124;
+let int1 = 51;
+let int2 = 84;
 
-if (int1 >= 50 && int1 <= 99) {
-  console.log("true");
+if (int1 >= 50 && int1 <= 99 && int2 >= 50 && int1 <= 99) {
+  console.log("Both are true");
+} else if (int1 >= 50 && int1 <= 99 && int2 < 50 && int1 > 99) {
+  console.log("The first one is true and the second one is false");
+} else if (int1 < 50 && int1 > 99 && int2 < 50 && int1 > 99) {
+  console.log("Both are false");
 } else {
-  console.log("false");
+  console.log("The first one is false and the second one is true");
 }
 
-if (int2 >= 50 && int1 <= 99) {
-  console.log("true");
-} else {
-  console.log("false");
-}
+// if (int2 >= 50 && int1 <= 99) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// EXERCISE 2
