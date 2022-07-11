a0f5520bea4c9dcd6c6b9a703de0486e161d219c
chris <chrishethertonweb@gmail.com>
2022-07-11 12:08:30 +0200

    exercise 3 done

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 let int1 = 41;
 let int2 = 91;
-let int3 = 1489;
+let int3 = -1489;
 
 if (int1 >= 50 && int1 <= 99 && int2 >= 50 && int2 <= 99) {
   console.log("Both are true");
@@ -36,3 +36,26 @@ if (
 }
 //   int1 < 50 && int1 > 99 && int2 < 50 && int2 > 99 && int3 < 50 && int3 > 99;
 else console.log("All are false");
+
+//     EXERCISE 3
+
+console.log("EXERCISE 3-----------------");
+let a = 33;
+let b = -456;
+let c = 122;
+
+if (a > b && a > c) {
+  console.log(a, "is the largest number");
+} else if (b > a && b > c) {
+  console.log(b, "is the largest number");
+} else {
+  console.log(c, "is the largest number");
+}
+
+if (int1 > int2 && int1 > int3) {
+  console.log(int1, "is the largest number");
+} else if (int2 > int1 && int2 > int3) {
+  console.log(int2, "is the largest number");
+} else {
+  console.log(int3, "is the largest number");
+}
