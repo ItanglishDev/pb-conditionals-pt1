f012a88116f5d7c1f01711c625f617c7455710d8
chris <chrishethertonweb@gmail.com>
2022-07-11 11:38:08 +0200

    1 done

---
+++ solution.js
@@ -0,0 +1,14 @@
+let int1 = 36;
+let int2 = 124;
+
+if (int1 >= 50 && int1 <= 99) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if (int2 >= 50 && int1 <= 99) {
+  console.log("true");
+} else {
+  console.log("false");
+}
